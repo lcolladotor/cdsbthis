0c38edbc72fda65b8625d87aa7496a7d98b4f9e8
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-07-16 18:39:16 -0400

    Actualizar ejemplos de cubicar() y agregar un stopifnot()

--- R/cubicar.R
+++ R/cubicar.R
@@ -1,13 +1,17 @@
 #' Calcula el cubo de un valor
 #'
-#' @param x un `numeric()` o `integer()` con el valor inicial. Puede ser un
+#' @param x un `numeric()` con el valor inicial. Puede ser un
 #' vector, en cuyo caso se usaran las reglas de reciclaje de R.
 #'
 #' @return El cubo del valor `x`
 #' @export
 #'
 #' @examples
+#'
+#' ## Unos ejemplos de cubicar()
 #' cubicar(10)
+#' cubicar(2)
 cubicar <- function(x) {
+    stopifnot(is.numeric(x))
     x^3
 }
